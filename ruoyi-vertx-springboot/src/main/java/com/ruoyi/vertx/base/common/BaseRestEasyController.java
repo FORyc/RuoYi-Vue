7a92f0273b99148363198eaee5afef487bbff0d8
tixnauxntop <1282743091@qq.com>
2022-11-23 16:19:03 +0800

    提交controller规则

---
+++ ruoyi-vertx-springboot/src/main/java/com/ruoyi/vertx/base/common/BaseRestEasyController.java
@@ -0,0 +1,7 @@
+package com.ruoyi.vertx.base.common;
+
+/**
+ * restEasyController 基类
+ */
+public abstract class BaseRestEasyController {
+}
--- ruoyi-vertx-springboot/src/main/java/com/ruoyi/vertx/controller/TestController.java
+++ ruoyi-vertx-springboot/src/main/java/com/ruoyi/vertx/controller/TestController.java
@@ -1,28 +1,35 @@
 package com.ruoyi.vertx.controller;
 
+import com.ruoyi.vertx.base.common.BaseRestEasyController;
 import io.vertx.core.http.HttpServerRequest;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.container.AsyncResponse;
+import jakarta.ws.rs.container.Suspended;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
 
+/**
+ * 新增的restController需要继承BaseRestEasyController，且加入Spring的管理
+ */
+@Component
 @Path(value = "/test")
-public class TestController {
+public class TestController extends BaseRestEasyController {
 
     private final Logger logger = Logger.getLogger(TestController.class);
 
     @GET
     @Path(value = "/get")
     @Produces(value = {MediaType.APPLICATION_JSON})
-    public String testGet(@Context HttpServerRequest request){
+    public AsyncResponse testGet(@Context HttpServerRequest request,
+                          @Suspended AsyncResponse asyncResponse){
         String name = request.getParam("name");
         logger.info("name:" + name);
-        if(name != null){
-            logger.info("namespace");
-        }
-        return name;
+        asyncResponse.resume(name);
+        return asyncResponse;
     }
 
 }
