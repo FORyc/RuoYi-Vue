8e69d5d6db90d4d17b871de9a5e5682a42116503
tixnauxntop <1282743091@qq.com>
2022-11-24 12:10:49 +0800

    新增配置

--- ruoyi-vertx-springboot/src/main/java/com/ruoyi/vertx/config/VertxConfig.java
+++ ruoyi-vertx-springboot/src/main/java/com/ruoyi/vertx/config/VertxConfig.java
@@ -1,6 +1,7 @@
 package com.ruoyi.vertx.config;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.WorkerExecutor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +11,10 @@ public class VertxConfig {
     @Bean
     public Vertx vertx(){
         return Vertx.vertx();
+    }
+
+    @Bean
+    public WorkerExecutor workerExecutor(Vertx vertx){
+        return vertx.createSharedWorkerExecutor("my-worker-pool", Runtime.getRuntime().availableProcessors());
     }
 }
--- ruoyi-vertx-springboot/src/main/java/com/ruoyi/vertx/verticle/ResteasyVerticle.java
+++ ruoyi-vertx-springboot/src/main/java/com/ruoyi/vertx/verticle/ResteasyVerticle.java
@@ -5,9 +5,8 @@ import io.vertx.core.AbstractVerticle;
 import org.apache.log4j.Logger;
 import org.jboss.resteasy.plugins.server.vertx.VertxRequestHandler;
 import org.jboss.resteasy.plugins.server.vertx.VertxResteasyDeployment;
-import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class ResteasyVerticle extends AbstractVerticle {
 
     private final Logger logger = Logger.getLogger(ResteasyVerticle.class);
