443ca14bbe41aebdf7bfc42ed6ad045a4f476206
tianxuantop <1282743091@qq.com>
2022-11-30 16:47:09 +0800

    修改获取spring管理的verticle的类的方式

--- ruoyi-vertx-springboot/src/main/java/com/ruoyi/vertx/listener/VertxSpringContextListener.java
+++ ruoyi-vertx-springboot/src/main/java/com/ruoyi/vertx/listener/VertxSpringContextListener.java
@@ -3,6 +3,7 @@ package com.ruoyi.vertx.listener;
 import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.extra.spring.SpringUtil;
+import com.ruoyi.vertx.base.annotation.Verticle;
 import com.ruoyi.vertx.base.constant.VerticleName;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
@@ -34,34 +35,38 @@ public class VertxSpringContextListener {
     @EventListener(classes = ApplicationStartedEvent.class)
     public void deploy(){
         logger.info("-------verticle 开始部署------");
-        Map<String, AbstractVerticle> verticleMap = SpringUtil.getBeansOfType(AbstractVerticle.class);
+        Map<String, Object> verticleMap = SpringUtil.getApplicationContext().getBeansWithAnnotation(Verticle.class);
         if (CollectionUtil.isNotEmpty(verticleMap)) {
-            for (Map.Entry<String, AbstractVerticle> entry : verticleMap.entrySet()) {
+            for (Map.Entry<String, Object> entry : verticleMap.entrySet()) {
                 try {
-                    if (entry.getKey().equals(VerticleName.WORK_VERTICLE)) {
-                        DeploymentOptions deploymentOptions = new DeploymentOptions();
-                        deploymentOptions.setWorker(true);
-                        vertx.deployVerticle(entry.getValue(), deploymentOptions, handler -> {
-                            if (handler.succeeded()) {
-                                logger.info("verticle[" + entry.getKey() + "] deployed");
-                            } else {
-                                logger.error(StrUtil.format("error deploying {}", handler.cause()), handler.cause());
-                            }
-                        });
+                    if (entry.getValue() instanceof AbstractVerticle){
+                        AbstractVerticle verticle = (AbstractVerticle) entry.getValue();
+                        if (entry.getKey().equals(VerticleName.WORK_VERTICLE)) {
+                            DeploymentOptions deploymentOptions = new DeploymentOptions();
+                            deploymentOptions.setWorker(true);
+                            vertx.deployVerticle(verticle, deploymentOptions, handler -> {
+                                if (handler.succeeded()) {
+                                    logger.info("verticle[" + entry.getKey() + "] deployed");
+                                } else {
+                                    logger.error(StrUtil.format("error deploying {}", handler.cause()), handler.cause());
+                                }
+                            });
+                        } else {
+                            vertx.deployVerticle(verticle, handler ->{
+                                if (handler.succeeded()) {
+                                    logger.info("verticle[" + entry.getKey() + "] deployed");
+                                } else {
+                                    logger.error(StrUtil.format("error deploying {}", handler.cause()), handler.cause());
+                                }
+                            });
+                        }
                     } else {
-                        vertx.deployVerticle(entry.getValue(), handler ->{
-                            if (handler.succeeded()) {
-                                logger.info("verticle[" + entry.getKey() + "] deployed");
-                            } else {
-                                logger.error(StrUtil.format("error deploying {}", handler.cause()), handler.cause());
-                            }
-                        });
+                        logger.warn(StrUtil.format("bean = [{}] 不是 AbstractVerticle，暂时不支持直接部署", entry.getKey()));
                     }
                 } catch (Exception e){
                     logger.warn(StrUtil.format("[{}]部署失败，msg = {}", entry.getValue(), e));
                 }
             }
-            logger.info("------verticle 部署完成-----");
         } else {
             logger.warn("未找到spring管理的verticle, vertx关闭...");
             vertx.close();
